4d1820cd0ab062c51290f216f754b20fa5eea99f
caiyazhou <yazhoucai@163.com>
2021-09-07 15:08:39 +0800

    feat: 🎸 完成h264视频流解密

--- demo/.vitepress/config.js
+++ demo/.vitepress/config.js
@@ -1,16 +1,12 @@
 module.exports = {
-    title: 'Jessibuca',
-    description: '一款纯H5直播流播放器',
-    themeConfig: {
-        repo: "langhuihui/jessibuca",
-        docsBranch: "v2",
-        sidebar: "auto",
-        nav:[
-            { text: 'API', link: '/api' },
-        ],
-        logo: 'logo.png'
-    },
-    head: [
-        ['script', {src: '/index.js'}]
-    ]
-}
+  title: "Jessibuca",
+  description: "一款纯H5直播流播放器",
+  themeConfig: {
+    repo: "langhuihui/jessibuca",
+    docsBranch: "v2",
+    sidebar: "auto",
+    nav: [{ text: "API", link: "/api" }],
+    logo: "logo.png",
+  },
+  head: [["script", { src: "/index.js" }]],
+};
